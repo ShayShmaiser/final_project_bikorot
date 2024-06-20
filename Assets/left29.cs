a8a3c05aa8fc080ff4be2ae5f1b73b7b1e02aa10
shayshmaiser209@gmail.com <shayshmaiser209@gmail.com>
2024-06-20 12:04:51 +0300

    Assets, Fifth commit: adding 16 more files

---
+++ Assets/left29.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class left29 : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawnPoint;
+    [SerializeField] private BoxCollider A;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        over();
+    }
+    public void over()
+    {
+        SceneManager.LoadScene("ending");
+    }
+}
